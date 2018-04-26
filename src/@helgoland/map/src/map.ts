5f034238669de335e6f0ea0dfd7fc01633c2db95
Jan Schulte <j.schulte@52north.org>
2018-04-26 12:58:07 +0200

    fix error in cached-map-component

--- src/@helgoland/map/src/map.ts
+++ src/@helgoland/map/src/map.ts
@@ -1,6 +1,6 @@
 export { HelgolandMapModule } from './module';
 export { MapCache } from './map-cache.service';
 export { LayerOptions } from './map-options';
-export { CachedMapComponent } from './cache-map-component';
+export { CachedMapComponent } from './cached-map-component';
 export { GeoSearch, GeoSearchOptions, GeoSearchResult, NominatimGeoSearchService } from './geosearch/index';
 export * from './geocure/geocure-layer';
