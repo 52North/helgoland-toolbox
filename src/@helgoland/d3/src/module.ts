663759a917a6e861881cb212ee65629c872d3603
Torben <t.kraf03@gmail.com>
2018-05-29 13:08:03 +0200

    start converting graph overview to d3

--- src/@helgoland/d3/src/module.ts
+++ src/@helgoland/d3/src/module.ts
@@ -3,18 +3,21 @@ import { HelgolandCoreModule } from '@helgoland/core';
 
 import { D3TrajectoryGraphComponent } from './d3-trajectory-graph/d3-trajectory-graph.component';
 import { D3TimeseriesGraphComponent } from './d3-timeseries-graph/d3-timeseries-graph.component';
+import { D3OverviewTimeseriesGraphComponent } from './d3-overview-timeseries-graph/d3-overview-timeseries-graph.component';
 
 @NgModule({
   declarations: [
     D3TrajectoryGraphComponent,
-    D3TimeseriesGraphComponent
+    D3TimeseriesGraphComponent,
+    D3OverviewTimeseriesGraphComponent
   ],
   imports: [
     HelgolandCoreModule
   ],
   exports: [
     D3TrajectoryGraphComponent,
-    D3TimeseriesGraphComponent
+    D3TimeseriesGraphComponent,
+    D3OverviewTimeseriesGraphComponent
   ],
   providers: []
 })
