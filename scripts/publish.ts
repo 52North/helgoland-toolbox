8c8dcbc7956329f098a14668781d91bc162b26e2
Jan Schulte <j.schulte@52north.org>
2018-06-18 16:10:36 +0200

    add publish script

---
+++ scripts/publish.ts
@@ -0,0 +1,15 @@
+import { exec, execSync } from 'child_process';
+
+import { modules } from './utils';
+
+execSync('npm addUser', { stdio: 'inherit' });
+
+modules.forEach(p => {
+  exec('npm publish dist/helgoland/' + p + ' --access public', (error: Error | null, stdout: string, stderr: string) => {
+    if (stderr) {
+      console.log(stderr);
+    } else {
+      console.log('Publish module \'' + p + '\'');
+    }
+  });
+});
