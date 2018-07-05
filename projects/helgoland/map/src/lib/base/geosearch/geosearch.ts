9880a5e63c6b94ac8fe6f6a199e351987670c949
Jan Schulte <j.schulte@52north.org>
2018-07-05 10:43:43 +0200

    add reverse geo search

--- projects/helgoland/map/src/lib/base/geosearch/geosearch.ts
+++ projects/helgoland/map/src/lib/base/geosearch/geosearch.ts
@@ -1,3 +1,4 @@
+import { Point } from 'geojson';
 import { LatLngBoundsLiteral } from 'leaflet';
 import { Observable } from 'rxjs/Observable';
 
@@ -9,10 +10,36 @@ export interface GeoSearchResult {
 
 export interface GeoSearchOptions {
     countrycodes?: string[];
+    asPointGeometry?: boolean;
+}
+
+export interface GeoReverseOptions { }
+
+export interface GeoReverseResult {
+    lat: string;
+    lon: string;
+    displayName?: string;
+    address?: {
+        houseNumber: string;
+        road: string;
+        neighbourhood: string;
+        suburb: string;
+        cityDistrict: string;
+        city: string;
+        county: string;
+        stateDistrict: string;
+        state: string;
+        postcode: string;
+        country: string;
+        countryCode: string;
+    };
+    boundingbox?: string[];
 }
 
 export abstract class GeoSearch {
 
     public abstract searchTerm(term: string, options?: GeoSearchOptions): Observable<GeoSearchResult>;
+
+    public abstract reverse(point: Point, options?: GeoReverseOptions): Observable<GeoReverseResult>;
 
 }
