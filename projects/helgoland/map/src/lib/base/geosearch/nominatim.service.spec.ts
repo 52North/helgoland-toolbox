c2c609068cf3674d221f4711b329adfe61f8d6f3
Jan Schulte <j.schulte@52north.org>
2018-07-05 10:43:43 +0200

    add reverse geo search

---
+++ projects/helgoland/map/src/lib/base/geosearch/nominatim.service.spec.ts
@@ -0,0 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
+import { inject, TestBed } from '@angular/core/testing';
+import { Point } from 'geojson';
+
+import { NominatimGeoSearchService } from './nominatim.service';
+
+describe('NominatimService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      providers: [NominatimGeoSearchService]
+    });
+  });
+
+  it('should be created', inject([NominatimGeoSearchService], (service: NominatimGeoSearchService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('should search with point geometry in result', inject([NominatimGeoSearchService], (service: NominatimGeoSearchService) => {
+    service.searchTerm('gent', {
+      asPointGeometry: true
+    }).subscribe(res => {
+      expect(res.geometry.type === 'Point').toBeTruthy();
+    });
+  }));
+
+  it('should reverse search', inject([NominatimGeoSearchService], (service: NominatimGeoSearchService) => {
+    const point: Point = {
+      type: 'Point',
+      coordinates: [51.9350437, 7.6520628]
+    };
+    service.reverse(point).subscribe(res => {
+        expect(res.address).toBeTruthy();
+    });
+  }));
+
+});
---
+++ projects/helgoland/map/src/lib/base/geosearch/nominatim.service.ts
@@ -0,0 +1,135 @@
+import 'rxjs/add/operator/map';
+
+import { HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpService } from '@helgoland/core';
+import { Point } from 'geojson';
+import { Observable } from 'rxjs/Observable';
+
+import { GeoReverseOptions, GeoReverseResult, GeoSearch, GeoSearchOptions, GeoSearchResult } from './geosearch';
+
+interface NominatimSearchResult {
+    display_name: string;
+    geojson?: GeoJSON.GeoJsonObject;
+    lat: string;
+    lon: string;
+    boundingbox: number[];
+}
+
+export interface Address {
+    address29: string;
+    house_number: string;
+    road: string;
+    neighbourhood: string;
+    suburb: string;
+    city_district: string;
+    city: string;
+    county: string;
+    state_district: string;
+    state: string;
+    postcode: string;
+    country: string;
+    country_code: string;
+}
+
+export interface NominatimReverseResult {
+    place_id: string;
+    licence: string;
+    osm_type: string;
+    osm_id: string;
+    lat: string;
+    lon: string;
+    display_name: string;
+    address: Address;
+    boundingbox: string[];
+}
+
+@Injectable()
+export class NominatimGeoSearchService implements GeoSearch {
+
+    private nominatimUrl = 'https://nominatim.openstreetmap.org/';
+
+    constructor(
+        protected http: HttpService
+    ) { }
+
+    public searchTerm(term: string, options?: GeoSearchOptions): Observable<GeoSearchResult> {
+        let params = new HttpParams();
+        params = params.set('limit', '1');
+        params = params.set('polygon_geojson', options.asPointGeometry ? '0' : '1');
+        params = params.set('q', term);
+        params = params.set('format', 'json');
+        if (options && options.countrycodes) { params = params.set('countrycodes', options.countrycodes.join(',')); }
+        return this.http.client().get(
+            this.nominatimUrl + 'search',
+            { params }
+        ).map((resArray: NominatimSearchResult[]) => {
+            if (resArray.length === 1) {
+                const result = resArray[0];
+                const name = result.display_name;
+                let bounds: Array<[number, number]>;
+                if (result.boundingbox) {
+                    bounds = [
+                        [
+                            result.boundingbox[0],
+                            result.boundingbox[2]
+                        ],
+                        [
+                            result.boundingbox[1],
+                            result.boundingbox[3]
+                        ]
+                    ];
+                } else {
+                    bounds = null;
+                }
+                let geometry;
+                if (result.geojson) {
+                    geometry = result.geojson;
+                } else {
+                    geometry = {
+                        type: 'Point',
+                        coordinates: [parseInt(result.lat, 10), parseInt(result.lon, 10)]
+                    };
+                }
+                return {
+                    name,
+                    geometry,
+                    bounds
+                };
+            }
+        });
+    }
+
+    public reverse(point: Point, options?: GeoReverseOptions): Observable<GeoReverseResult> {
+        let params = new HttpParams();
+        params = params.set('lat', point.coordinates[0].toString());
+        params = params.set('lon', point.coordinates[1].toString());
+        params = params.set('format', 'json');
+        return this.http.client().get(
+            this.nominatimUrl + 'reverse',
+            { params }
+        ).map((res: NominatimReverseResult) => {
+            return {
+                lat: res.lat,
+                lon: res.lon,
+                displayName: res.display_name,
+                address: {
+                    city: res.address.city,
+                    cityDistrict: res.address.city_district,
+                    country: res.address.country,
+                    countryCode: res.address.country_code,
+                    county: res.address.county,
+                    houseNumber: res.address.house_number,
+                    neighbourhood: res.address.neighbourhood,
+                    postcode: res.address.postcode,
+                    road: res.address.road,
+                    state: res.address.state,
+                    stateDistrict: res.address.state_district,
+                    suburb: res.address.suburb
+                },
+                boundingbox: res.boundingbox
+            } as GeoReverseResult;
+        });
+    }
+
+}
