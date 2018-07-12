b721eaf1d11c0d661bc2181298dba48a15f97304
Torben <t.kraf03@gmail.com>
2018-07-12 14:27:41 +0200

    add description of plot and dataset options, adapt d3 component to options

--- projects/helgoland/d3/src/lib/model/d3-plot-options.ts
+++ projects/helgoland/d3/src/lib/model/d3-plot-options.ts
@@ -1,7 +1,19 @@
+/**
+ * Plot options for D3 component.
+ * @param showReferenceValues {boolean} show reference values for a graph
+ * @param generalizeAllways {boolean} generalize
+ * @param togglePanZoom {boolean} toggle panning (true) and zooming (false)
+ * @param yaxis {boolean} show or hide y axis
+ * @param grid {boolean} show or hide grid lines inside plot
+ * @param hoverable {boolean} show or hide lines with values when hovering with mouse
+ * @param overview {boolean} indicating if component should build overview diagram or not
+ */
 export interface D3PlotOptions {
     showReferenceValues?: boolean;
     generalizeAllways?: boolean;
     togglePanZoom?: boolean;
     yaxis?: boolean;
-    grid?: any;
+    grid?: boolean;
+    hoverable?: boolean;
+    overview?: boolean;
 }
