dc05457cdc2bef303a7683bb4ce7ef656ee6e268
Jan Schulte <j.schulte@52north.org>
2018-09-06 13:50:22 +0200

    fix panning error in extended data d3 component

--- projects/helgoland/d3/src/lib/d3-timeseries-graph/d3-timeseries-graph.component.ts
+++ projects/helgoland/d3/src/lib/d3-timeseries-graph/d3-timeseries-graph.component.ts
@@ -529,10 +529,7 @@ export class D3TimeseriesGraphComponent
 
         if (setDataExtent) {
             calculatedRange = { min: dataExtent[0], max: dataExtent[1] };
-            if (calculatedRange.min === calculatedRange.max) {
-                calculatedRange.min = calculatedRange.min - (calculatedRange.min * 0.1);
-                calculatedRange.max = calculatedRange.max + (calculatedRange.max * 0.1);
-            }
+            this.extendRange(calculatedRange);
         }
 
         // if style option 'zero based y-axis' is checked,
@@ -618,6 +615,13 @@ export class D3TimeseriesGraphComponent
         });
         if (this.graph) {
             this.plotGraph();
+        }
+    }
+
+    protected extendRange(range: MinMaxRange) {
+        if (range.min === range.max) {
+            range.min = range.min - 1;
+            range.max = range.max + 1;
         }
     }
 
@@ -955,24 +959,6 @@ export class D3TimeseriesGraphComponent
     }
 
     /**
-     * Function that returns the tickvalues for the x axis related to the timestep.
-     * @param time {String} String with the information how the time should be visualized in the x axis.
-     * @param range {Array} Array containing the minimum and maximum range.
-     * @param span {Number} Number with the ticksize for the axis generation.
-     */
-    private timeTickValues(time: String, range: any, span: number) {
-        if (time === 'minly') {
-            return d3.timeMinutes(range, new Date(this.xAxisRange.to), span);
-        }
-        if (time === 'hourly') {
-            return d3.timeHours(range, new Date(this.xAxisRange.to), span);
-        }
-        if (time === 'daily') {
-            return d3.timeDay.range(range, new Date(this.xAxisRange.to), span);
-        }
-    }
-
-    /**
      * Function that draws the x axis to the svg element.
      * @param bufferXrange {Number} Number with the distance between left edge and the beginning of the graph.
      */
@@ -991,7 +977,7 @@ export class D3TimeseriesGraphComponent
                     formatMinute = '%H:%M',
                     formatHour = '%H:%M',
                     formatDay = '%a %d',
-                    formatWeek = '%b %d',
+                    formatWeek = '%a %d',
                     formatMonth = '%B',
                     formatYear = '%Y';
 
