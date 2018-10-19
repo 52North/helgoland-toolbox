66fa20a92a943479e19cbe9bc4481d8a4ef58a10
Jan Schulte <j.schulte@52north.org>
2018-10-19 14:25:52 +0200

    improve color service

--- projects/helgoland/core/src/lib/color/color.service.ts
+++ projects/helgoland/core/src/lib/color/color.service.ts
@@ -3,8 +3,26 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ColorService {
 
+    /**
+     * Creates a random color and return it as a hex string.
+     * @returns {string}
+     */
     public getColor(): string {
         return this.getRandomColor();
+    }
+
+    /**
+     * Converts a hex string and opacity in percent to RGBA color as string.
+     * @param {string} hex
+     * @param {number} opacity
+     * @returns {string}
+     */
+    public convertHexToRGBA(hex: string, opacity: number): string {
+        hex = hex.replace('#', '');
+        const r = parseInt(hex.substring(0, 2), 16);
+        const g = parseInt(hex.substring(2, 4), 16);
+        const b = parseInt(hex.substring(4, 6), 16);
+        return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')';
     }
 
     private getRandomColor(): string {
