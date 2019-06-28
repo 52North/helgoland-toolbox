8ac4c09801efb89fca7d7a36bf898fb3bd983765
Jan Schulte <j.schulte@52north.org>
2019-06-28 11:55:32 +0200

    adjust tests and travis config

--- scripts/test.ts
+++ scripts/test.ts
@@ -4,5 +4,5 @@ import { modules } from './utils';
 
 modules.forEach(p => {
   console.log('##### Test helgoland-toolbox \'' + p + '\' module #####');
-  execSync('ng test @helgoland/' + p + ' --watch=false', { stdio: [0, 1, 2] });
+  execSync('ng test @helgoland/' + p + ' --watch=false --browsers=ChromeHeadless', { stdio: [0, 1, 2] });
 });
