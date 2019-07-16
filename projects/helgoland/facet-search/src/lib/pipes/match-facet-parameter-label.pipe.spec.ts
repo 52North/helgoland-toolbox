a407cfcd5cbdfcb84acfddb14abbcfdce4ae729c
Jan Schulte <j.schulte@52north.org>
2019-07-16 16:38:07 +0200

    add facet-search lib

---
+++ projects/helgoland/facet-search/src/lib/pipes/match-facet-parameter-label.pipe.spec.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-unused-variable */
+import { MatchFacetParameterLabelPipe } from './match-facet-parameter-label.pipe';
+
+describe('MatchFacetParameterLabelPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MatchFacetParameterLabelPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
