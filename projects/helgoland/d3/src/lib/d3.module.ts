e51b5ade01ff6699ee1641ed6a507063c9297e97
Jan Schulte <j.schulte@52north.org>
2019-08-02 14:55:10 +0200

    add export button

--- projects/helgoland/d3/src/lib/d3.module.ts
+++ projects/helgoland/d3/src/lib/d3.module.ts
@@ -1,32 +1,34 @@
 import { NgModule } from '@angular/core';
 import { HelgolandCoreModule } from '@helgoland/core';
 
+import { D3GeneralGraphComponent } from './d3-general-graph/d3-general-graph.component';
 import { D3OverviewTimeseriesGraphComponent } from './d3-overview-timeseries-graph/d3-overview-timeseries-graph.component';
 import { D3TimeseriesGraphComponent } from './d3-timeseries-graph/d3-timeseries-graph.component';
 import { D3TrajectoryGraphComponent } from './d3-trajectory-graph/d3-trajectory-graph.component';
+import { ExportImageButtonComponent } from './export-image-button/export-image-button.component';
 import {
   ExtendedDataD3TimeseriesGraphComponent,
 } from './extended-data-d3-timeseries-graph/extended-data-d3-timeseries-graph.component';
 import { D3TimeFormatLocaleService } from './helper/d3-time-format-locale.service';
-import { D3GeneralGraphComponent } from './d3-general-graph/d3-general-graph.component';
+
+const COMPONENTS = [
+  D3TrajectoryGraphComponent,
+  D3TimeseriesGraphComponent,
+  D3OverviewTimeseriesGraphComponent,
+  ExtendedDataD3TimeseriesGraphComponent,
+  D3GeneralGraphComponent,
+  ExportImageButtonComponent
+];
 
 @NgModule({
   declarations: [
-    D3TrajectoryGraphComponent,
-    D3TimeseriesGraphComponent,
-    D3OverviewTimeseriesGraphComponent,
-    ExtendedDataD3TimeseriesGraphComponent,
-    D3GeneralGraphComponent
+    COMPONENTS
   ],
   imports: [
     HelgolandCoreModule
   ],
   exports: [
-    D3TrajectoryGraphComponent,
-    D3TimeseriesGraphComponent,
-    D3OverviewTimeseriesGraphComponent,
-    ExtendedDataD3TimeseriesGraphComponent,
-    D3GeneralGraphComponent
+    COMPONENTS
   ],
   providers: [
     D3TimeFormatLocaleService
