c0717569a73bf74cc45dc3e96314aa12ef9dfc6b
Jan Schulte <j.schulte@52north.org>
2019-08-02 14:55:10 +0200

    add export button

--- projects/helgoland/d3/src/public_api.ts
+++ projects/helgoland/d3/src/public_api.ts
@@ -8,6 +8,7 @@ export { D3TimeseriesGraphComponent } from './lib/d3-timeseries-graph/d3-timeser
 export { D3GeneralGraphComponent } from './lib/d3-general-graph/d3-general-graph.component';
 export { D3OverviewTimeseriesGraphComponent } from './lib/d3-overview-timeseries-graph/d3-overview-timeseries-graph.component';
 export { AdditionalData, ExtendedDataD3TimeseriesGraphComponent } from './lib/extended-data-d3-timeseries-graph/extended-data-d3-timeseries-graph.component';
+export { ExportImageButtonComponent } from './lib/export-image-button/export-image-button.component';
 export * from './lib/model/d3-graph-options';
 export * from './lib/model/d3-axis-type';
 export * from './lib/model/d3-highlight';
