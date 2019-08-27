0560da4470cd66244d93535938af5968c48bdb3e
Jan Schulte <j.schulte@52north.org>
2019-08-27 11:15:20 +0200

    fix dataset options modification error in d3 component

--- projects/helgoland/d3/src/lib/d3-timeseries-graph/d3-timeseries-graph.component.ts
+++ projects/helgoland/d3/src/lib/d3-timeseries-graph/d3-timeseries-graph.component.ts
@@ -194,6 +194,7 @@ export class D3TimeseriesGraphComponent
     };
     private addLineWidth = 2; // value added to linewidth
     private loadingCounter = 0;
+    private loadingData: Set<string> = new Set();
     private currentTimeId: string;
 
     // default plot options
@@ -387,7 +388,8 @@ export class D3TimeseriesGraphComponent
         if (dataset instanceof Timeseries) {
             const buffer = this.timeSrvc.getBufferedTimespan(this.timespan, 0.2);
 
-            datasetOptions.loadData = true;
+            this.loadingData.add(dataset.internalId);
+            this.dataLoaded.emit(this.loadingData);
             this.api.getTsData<[number, number]>(dataset.id, dataset.url, buffer,
                 {
                     format: 'flot',
@@ -398,13 +400,14 @@ export class D3TimeseriesGraphComponent
             ).subscribe(
                 (result) => this.prepareTsData(dataset, result),
                 (error) => this.onError(error),
-                () => this.onCompleteLoadingData(datasetOptions)
+                () => this.onCompleteLoadingData(dataset)
             );
         }
     }
 
-    private onCompleteLoadingData(options: DatasetOptions): void {
-        options.loadData = false;
+    private onCompleteLoadingData(dataset: IDataset): void {
+        this.loadingData.delete(dataset.internalId);
+        this.dataLoaded.emit(this.loadingData);
         this.loadingCounter--;
         if (this.loadingCounter === 0) { this.onContentLoading.emit(false); }
     }
