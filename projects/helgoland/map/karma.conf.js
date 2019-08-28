de157064c9dfd549c667156787d292c862455f42
Jan Schulte <j.schulte@52north.org>
2019-08-28 16:15:55 +0200

    add karma test parameter

--- projects/helgoland/map/karma.conf.js
+++ projects/helgoland/map/karma.conf.js
@@ -38,6 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    browserNoActivityTimeout : 60000
   });
 };
