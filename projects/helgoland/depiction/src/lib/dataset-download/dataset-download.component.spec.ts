40070f290cdbb75c76e91c8d4733d8e188ff0346
Torben <t.kraf03@gmail.com>
2019-08-28 14:10:00 +0200

    fix test (csv download), add csv download to graph.legend in toolbox

--- projects/helgoland/depiction/src/lib/dataset-download/dataset-download.component.spec.ts
+++ projects/helgoland/depiction/src/lib/dataset-download/dataset-download.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DatasetDownloadComponent } from './dataset-download.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/app.module';
+import { HttpClient } from '@angular/common/http';
+import { HelgolandCoreModule } from '@helgoland/core';
 
 describe('DatasetDownloadComponent', () => {
   let component: DatasetDownloadComponent;
@@ -8,7 +12,17 @@ describe('DatasetDownloadComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DatasetDownloadComponent ]
+      declarations: [ DatasetDownloadComponent ],
+      imports: [
+        HelgolandCoreModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ]
     })
     .compileComponents();
   }));
