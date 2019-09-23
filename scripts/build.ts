7cd2d60feebb66fed73174eef4bb68d3006b1cb6
Jan Schulte <j.schulte@52north.org>
2019-09-23 15:45:50 +0200

    upgrade deps

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,6 @@ import { execSync } from 'child_process';
 import { modules } from './utils';
 
 modules.forEach(p => {
-  console.log('##### Build helgoland-toolbox \'' + p + '\' module #####');
+  // console.log('##### Build helgoland-toolbox \'' + p + '\' module #####');
   execSync('ng build @helgoland/' + p, { stdio: [0, 1, 2] });
 });
