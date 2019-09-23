c0484a0650ad8723de05c7b299f6fdd5fa41bc8a
Jan Schulte <j.schulte@52north.org>
2019-09-23 16:06:17 +0200

    adjust scripts

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,7 +2,4 @@ import { execSync } from 'child_process';
 
 import { modules } from './utils';
 
-modules.forEach(p => {
-  // console.log('##### Build helgoland-toolbox \'' + p + '\' module #####');
-  execSync('ng build @helgoland/' + p, { stdio: [0, 1, 2] });
-});
+modules.forEach(p => execSync('ng build @helgoland/' + p, { stdio: [0, 1, 2] }));
