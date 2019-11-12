a37bfa21b597d5bb8b89af2be387e50655b291bd
Jan Schulte <j.schulte@52north.org>
2019-11-12 13:08:45 +0100

    add configurable caching duration to caching module

--- projects/helgoland/caching/src/lib/caching.module.ts
+++ projects/helgoland/caching/src/lib/caching.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
-import { HTTP_SERVICE_INTERCEPTORS, HelgolandCoreModule } from '@helgoland/core';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { HelgolandCoreModule, HTTP_SERVICE_INTERCEPTORS } from '@helgoland/core';
 
 import { CachingInterceptor } from './cache-interceptor';
 import { LocalHttpCache } from './local-http-cache';
 import { LocalOngoingHttpCache } from './local-ongoing-http-cache';
 import { HttpCache, OnGoingHttpCache } from './model';
+
+/**
+ * Configuration for the HelgolandCachingModule
+ */
+export interface CacheConfig {
+  /**
+   * Duration in milliseconds, how long equal request will be cached until refresh
+   */
+  cachingDurationInMilliseconds?: number;
+}
+
+export const CacheConfigService = new InjectionToken<CacheConfig>('CacheConfigService');
+
+const PROVIDERS = [
+  {
+    provide: HttpCache,
+    useClass: LocalHttpCache
+  },
+  {
+    provide: HTTP_SERVICE_INTERCEPTORS,
+    useClass: CachingInterceptor,
+    multi: true
+  },
+  {
+    provide: OnGoingHttpCache,
+    useClass: LocalOngoingHttpCache
+  }
+]
 
 @NgModule({
   declarations: [],
@@ -12,20 +40,20 @@ import { HttpCache, OnGoingHttpCache } from './model';
     HelgolandCoreModule
   ],
   exports: [],
-  providers: [
-    {
-      provide: HttpCache,
-      useClass: LocalHttpCache
-    },
-    {
-      provide: HTTP_SERVICE_INTERCEPTORS,
-      useClass: CachingInterceptor,
-      multi: true
-    },
-    {
-      provide: OnGoingHttpCache,
-      useClass: LocalOngoingHttpCache
-    },
-  ]
+  providers: PROVIDERS
 })
-export class HelgolandCachingModule { }
+export class HelgolandCachingModule {
+
+  static forRoot(config: CacheConfig): ModuleWithProviders {
+    return {
+      ngModule: HelgolandCachingModule,
+      providers: [
+        {
+          provide: CacheConfigService,
+          useValue: config
+        }
+      ]
+    };
+  }
+
+}
