89164ff3d7542e3cac07dd7c8c8507ab4948f189
Jan Schulte <j.schulte@52north.org>
2019-11-13 08:52:56 +0100

    fix tests

--- projects/helgoland/caching/src/lib/caching.module.ts
+++ projects/helgoland/caching/src/lib/caching.module.ts
@@ -1,22 +1,11 @@
-import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { HelgolandCoreModule, HTTP_SERVICE_INTERCEPTORS } from '@helgoland/core';
 
 import { CachingInterceptor } from './cache-interceptor';
+import { CacheConfig, CacheConfigService } from './config';
 import { LocalHttpCache } from './local-http-cache';
 import { LocalOngoingHttpCache } from './local-ongoing-http-cache';
 import { HttpCache, OnGoingHttpCache } from './model';
-
-/**
- * Configuration for the HelgolandCachingModule
- */
-export interface CacheConfig {
-  /**
-   * Duration in milliseconds, how long equal request will be cached until refresh
-   */
-  cachingDurationInMilliseconds?: number;
-}
-
-export const CacheConfigService = new InjectionToken<CacheConfig>('CacheConfigService');
 
 const PROVIDERS = [
   {
