b5b87d93b8f6f8b6ec4843e09ef3e7dbd7c442f3
Jan Schulte <j.schulte@52north.org>
2020-02-20 21:54:32 +0100

    add default settings service to providers

--- projects/helgoland/core/src/lib/core.module.ts
+++ projects/helgoland/core/src/lib/core.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 
 import { ColorService } from './color/color.service';
 import { DatasetApiMapping } from './dataset-api/api-mapping.service';
@@ -7,11 +7,21 @@ import { StatusIntervalResolverService } from './dataset-api/helper/status-inter
 import { HttpService } from './dataset-api/http.service';
 import { InternalIdHandler } from './dataset-api/internal-id-handler.service';
 import { LocalStorage } from './local-storage/local-storage.service';
+import { Settings } from './model/settings/settings';
 import { NotifierService } from './notifier/notifier.service';
 import { DateProxyPipe } from './pipes/dateproxy/dateproxy.pipe';
 import { SumValuesService } from './processing/sum-values.service';
+import { SettingsService } from './settings/settings.service';
 import { DefinedTimespanService } from './time/defined-timespan.service';
 import { Time } from './time/time.service';
+
+@Injectable()
+export class ExtendedSettingsService extends SettingsService<Settings> {
+  constructor() {
+    super();
+    this.setSettings({});
+  }
+}
 
 @NgModule({
   declarations: [
@@ -33,7 +43,11 @@ import { Time } from './time/time.service';
     StatusIntervalResolverService,
     SumValuesService,
     HttpService,
-    Time
+    Time,
+    {
+      provide: SettingsService,
+      useClass: ExtendedSettingsService
+    }
   ]
 })
 export class HelgolandCoreModule { }
