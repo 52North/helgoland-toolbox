58e54a6abb4da5b7cc943bf1300fcab8bc785f6d
Jan Schulte <j.schulte@52north.org>
2020-05-05 08:19:31 +0200

    adjust test

--- projects/helgoland/core/src/lib/dataset-api/internal-id-handler.service.spec.ts
+++ projects/helgoland/core/src/lib/dataset-api/internal-id-handler.service.spec.ts
@@ -2,7 +2,7 @@ import { inject, TestBed } from '@angular/core/testing';
 
 import { InternalIdHandler } from './internal-id-handler.service';
 
-fdescribe('InternalIdHandler', () => {
+describe('InternalIdHandler', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [InternalIdHandler]
