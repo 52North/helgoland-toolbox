b49aa8ebb6c700ca318f639cb80ba045663a2264
Jan Schulte <j.schulte@52north.org>
2020-05-29 14:00:09 +0200

    fix time label axis formatting

--- projects/helgoland/d3/src/lib/helper/d3-time-format-locale.service.ts
+++ projects/helgoland/d3/src/lib/helper/d3-time-format-locale.service.ts
@@ -3,21 +3,7 @@ import { TimezoneService } from '@helgoland/core';
 import moment from 'moment';
 
 /**
- * This service holds the translations for d3 charts time axis labels.
- * Add a new translation with the method 'addTimeFormatLocale' like this sample:
- *
- * addTimeFormatLocale('de',
- * {
- *   'dateTime': '%a %b %e %X %Y',
- *   'date': '%d-%m-%Y',
- *   'time': '%H:%M:%S',
- *   'periods': ['AM', 'PM'],
- *   'days': ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
- *   'shortDays': ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
- *   'months': ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
- *   'shortMonths': ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
- * })
- *
+ * This service formats the time labels for the time axis in d3. Internationalisation will be managed by moment.
  */
 @Injectable({
   providedIn: 'root'
@@ -48,7 +34,7 @@ export class D3TimeFormatLocaleService {
               : this.roundYear(time) < curr ? this.formatMonth
                 : this.formatYear;
 
-    return moment(time).tz(this.timezoneSrvc.getTimezoneName()).format(format);
+    return this.timezoneSrvc.formatTzDate(moment(time), format);
   }
 
   private roundMinute(time: number) {
