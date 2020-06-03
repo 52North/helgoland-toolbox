68fc31f605ad746a598f369323a03f7124399522
Jan Schulte <j.schulte@52north.org>
2020-06-03 13:44:43 +0200

    add copy readme to build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,10 @@
 import { execSync } from 'child_process';
+import { copyFileSync } from 'fs';
 
 import { modules } from './utils';
 
-modules.forEach(p => execSync(`ng build @helgoland/${p} --prod`, { stdio: [0, 1, 2] }));
+modules.forEach(p => {
+    execSync(`ng build @helgoland/${p} --prod`, { stdio: [0, 1, 2] });
+    copyFileSync('README.md', `dist/helgoland/${p}/README.md`);
+});
 
