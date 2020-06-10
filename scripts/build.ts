0c4b36c12d4e780cdcd3829413e2a18e4ec964f0
Jan Schulte <j.schulte@52north.org>
2020-06-10 09:25:05 +0200

    adjust build and pack script

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,7 +4,6 @@ import { copyFileSync } from 'fs';
 import { modules } from './utils';
 
 modules.forEach(p => {
-    execSync(`ng build @helgoland/${p} --prod`, { stdio: [0, 1, 2] });
-    copyFileSync('README.md', `dist/helgoland/${p}/README.md`);
+  execSync(`ng build ${p} --prod`, { stdio: [0, 1, 2] });
+  copyFileSync('README.md', `dist/libs/${p}/README.md`);
 });
-
