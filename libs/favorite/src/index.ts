e2abb9561a144b6e419f0bff88c9eae70197352c
Jan Schulte <j.schulte@52north.org>
2020-10-05 13:01:55 +0200

    add export favorite toggler component

--- libs/favorite/src/index.ts
+++ libs/favorite/src/index.ts
@@ -5,3 +5,4 @@
 export * from './lib/favorite.module';
 export * from './lib/service/favorite.service';
 export * from './lib/service/json-favorite-exporter.service';
+export * from './lib/favorite-toggler/favorite-toggler.component';
