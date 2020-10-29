a5fc08f600ceed7df5309dbe2e962dc334a92bd5
Jan Schulte <j.schulte@52north.org>
2020-10-29 12:56:40 +0100

    adjust app settings

--- apps/helgoland/jest.config.js
+++ apps/helgoland/jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  name: 'helgoland',
-  preset: '../../jest.config.js',
+  displayName: 'helgoland',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
     },
   },
   coverageDirectory: '../../coverage/apps/helgoland',
