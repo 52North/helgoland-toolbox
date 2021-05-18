cb9a9718659c507173de037a35d01fe75680d4ec
Jan Schulte <j.schulte@52north.org>
2021-05-18 10:48:22 +0200

    adjust lib versions

--- scripts/bumpVersions.ts
+++ scripts/bumpVersions.ts
@@ -2,11 +2,24 @@ import { readFile, writeFile } from 'jsonfile';
 
 import { modules } from './utils';
 
-readFile('./package.json', function (err, obj) {
+readFile('./package.json', (err, obj) => {
+  console.log(`bump to version ${obj.version}`);
+  // console.log(obj.dependencies);
   modules.forEach(p => {
     const packageFile = './libs/' + p + '/package.json';
-    readFile(packageFile, function (e, packageJson) {
+    readFile(packageFile, (e, packageJson) => {
       packageJson.version = obj.version;
+      // console.log(packageJson.dependencies);
+      for (const key in packageJson.dependencies) {
+        if (Object.prototype.hasOwnProperty.call(packageJson.dependencies, key)) {
+          // const element = packageJson.dependencies[key];
+          if (obj.dependencies[key]) {
+            packageJson.dependencies[key] = obj.dependencies[key];
+          } else {
+            console.error(`Could not found ${key} in main package.json`);
+          }
+        }
+      }
       writeFile('./libs/' + p + '/package.json', packageJson, { spaces: 2 }, (error) => {
         if (error) { console.error(error); }
       });
