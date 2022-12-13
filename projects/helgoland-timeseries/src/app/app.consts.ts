e5493e40c56ca4f5cba6f57385eba3d94b6362cc
Jan Schulte <j.schulte@52north.org>
2022-12-13 17:15:24 +0100

    adjust leaflet marker config

--- projects/helgoland-timeseries/src/app/app.consts.ts
+++ projects/helgoland-timeseries/src/app/app.consts.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { icon, Marker } from 'leaflet';
 
 import { DiagramViewComponent } from './views/diagram-view/diagram-view.component';
 
@@ -9,3 +10,15 @@ export const ROUTES: Routes = [
     component: DiagramViewComponent
   }
 ];
+
+
+Marker.prototype.options.icon = icon({
+  iconRetinaUrl: 'assets/img/marker-icon-2x.png',
+  iconUrl: 'assets/img/marker-icon.png',
+  shadowUrl: 'assets/img/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
