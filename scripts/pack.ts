7bc92e1ccd5dad8da4649212d3b53ee62be9eecd
Jan Schulte <j.schulte@52north.org>
2023-04-25 14:18:07 +0200

    improve pack script

--- scripts/pack.ts
+++ scripts/pack.ts
@@ -1,10 +1,12 @@
-import { exec } from 'child_process';
+import { exec, execSync } from 'child_process';
 
 import { modules } from './utils';
 
+exec('rm -rf ../npm-builds/ && mkdir ../npm-builds');
 modules.forEach(p => {
   exec('npm pack dist/helgoland/' + p, (error: Error | null, stdout: string, stderr: string) => {
-    console.log('Packed module \'' + p + '\'');
-    exec('mv helgoland-* ../npm-builds -f');
+    const fileName = stdout.trim();
+    console.log(`Packed module '${fileName}'`);
+    execSync(`mv ${fileName} ../npm-builds/${fileName} -f`);
   });
 });
