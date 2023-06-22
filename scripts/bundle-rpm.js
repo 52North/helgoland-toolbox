b87e8864e296fc12463f8d4b70f901d5ec6409f6
Henning Bredel <h.bredel@52north.org>
2023-06-22 16:36:51 +0200

    Build with configurable path and  base-url

--- scripts/bundle-rpm.js
+++ scripts/bundle-rpm.js
@@ -1,8 +1,21 @@
+const { execSync } = require('child_process');
 const buildRpm = require('rpm-builder');
 
 // Jenkins provides a build number
 const buildNumber = process.env.BUILD_NUMBER || "1";
- 
+
+
+if (process.argv.length > 2) {
+  if (process.argv[2]) {
+    appname = process.argv[2];
+  }
+}
+
+
+const application = process.argv[2] ?? "helgoland-timeseries";
+const targetName = process.argv[3] ?? application;
+const buildPath = `./dist/${targetName}/`;
+
 const options = {
   name: 'sensorwebclient',
   version: '15.0.0',
@@ -13,10 +26,16 @@ const options = {
   keepTemp: false,
   rpmDest: './dist',
   files: [
-    { cwd: './dist/helgoland-timeseries/', src: '*.*', dest: '/opt/edis/sensorwebclient/' }
+    { cwd: buildPath, src: '*.*', dest: `/opt/edis/${targetName}/` }
   ]
 };
- 
+
+console.log(`Build application ${application}`);
+execSync(
+  `rm -rf ${buildPath} && ng build ${application} --output-path=${buildPath} --base-href=/${targetName}/`,
+  { stdio: [0, 1, 2] }
+);
+
 buildRpm(options, function(err, rpm) {
   if (err) {
     throw err;
@@ -25,3 +44,4 @@ buildRpm(options, function(err, rpm) {
   console.log(`RPM file written to ${rpm}`);
   
 });
+ 
