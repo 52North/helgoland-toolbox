18f8618664f13b005b79047ea31bab20b5200669
Jan Schulte <j.schulte@52north.org>
2023-12-05 07:48:25 +0100

    refactoring

--- projects/helgoland/core/src/lib/model/dataset-api/dataset.ts
+++ projects/helgoland/core/src/lib/model/dataset-api/dataset.ts
@@ -4,61 +4,73 @@ import { Parameter } from "./parameter";
 import { Station } from "./station";
 
 export interface IDataset extends Parameter {
-    url: string;
-    uom: string;
-    internalId: string;
-    firstValue: FirstLastValue;
-    lastValue: FirstLastValue;
-    referenceValues: ReferenceValue[];
-    parameters: ParameterConstellation;
-    renderingHints: RenderingHints;
+  url: string;
+  uom: string;
+  internalId: string;
+  firstValue: FirstLastValue;
+  lastValue: FirstLastValue;
+  referenceValues: ReferenceValue[];
+  parameters: ParameterConstellation;
+  renderingHints: RenderingHints;
 }
 
 export class ParameterConstellation {
-  public service?: Parameter;
-  public offering?: Parameter;
-  public feature?: Feature;
-  public procedure?: Parameter;
-  public phenomenon?: Parameter;
-  public category?: Parameter;
+
+  constructor(
+    public service?: Parameter,
+    public offering?: Parameter,
+    public feature?: Feature,
+    public procedure?: Parameter,
+    public phenomenon?: Parameter,
+    public category?: Parameter
+  ) { }
+
 }
 
 export class FirstLastValue {
-  public timestamp!: number;
-  public value!: number;
+
+  constructor(
+    public timestamp: number,
+    public value: number
+  ) { }
+
 }
 
 export class ReferenceValue {
-  public referenceValueId!: string;
-  public label!: string;
-  public lastValue!: FirstLastValue;
-  public color?: string;
-  public visible?: boolean;
+
+  constructor(
+    public referenceValueId: string,
+    public label: string,
+    public lastValue?: FirstLastValue,
+    public color?: string,
+    public visible?: boolean
+  ) { }
+
 }
 
 export interface RenderingHints {
-    chartType: string;
-    properties: {
-        color: string;
-    };
+  chartType: string;
+  properties: {
+    color: string;
+  };
 }
 
 export interface LineRenderingHints extends RenderingHints {
-    chartType: "line";
-    properties: {
-        color: string;
-        width: string;
-        lineType: string;
-    };
+  chartType: "line";
+  properties: {
+    color: string;
+    width: string;
+    lineType: string;
+  };
 }
 
 export interface BarRenderingHints {
-    chartType: "bar";
-    properties: {
-        color: string;
-        width: string;
-        interval: string;
-    };
+  chartType: "bar";
+  properties: {
+    color: string;
+    width: string;
+    interval: string;
+  };
 }
 
 export class DatasetParameterConstellation extends ParameterConstellation {
@@ -66,55 +78,69 @@ export class DatasetParameterConstellation extends ParameterConstellation {
 }
 
 export class Dataset implements IDataset {
-  public id!: string;
-  public label!: string;
-  public url!: string;
-  public uom!: string;
-  public internalId!: string;
-  public firstValue!: FirstLastValue;
-  public lastValue!: FirstLastValue;
-  public referenceValues!: ReferenceValue[];
-  public datasetType!: DatasetTypes;
-  public platformType!: PlatformTypes;
-  public parameters!: DatasetParameterConstellation;
-  public renderingHints!: RenderingHints;
+
   public seriesParameters?: DatasetParameterConstellation;
+
+  constructor(
+    public id: string,
+    public label: string,
+    public url: string,
+    public uom: string,
+    public internalId: string,
+    public firstValue: FirstLastValue,
+    public lastValue: FirstLastValue,
+    public referenceValues: ReferenceValue[],
+    public datasetType: DatasetTypes,
+    public platformType: PlatformTypes,
+    public parameters: DatasetParameterConstellation,
+    public renderingHints: RenderingHints
+  ) { }
+
 }
 
 export class Timeseries implements IDataset {
-  public id!: string;
-  public label!: string;
-  public url!: string;
-  public uom!: string;
-  public internalId!: string;
-  public firstValue!: FirstLastValue;
-  public lastValue!: FirstLastValue;
-  public referenceValues!: ReferenceValue[];
-  public station!: Station;
-  public parameters!: ParameterConstellation;
-  public renderingHints!: RenderingHints;
+
   public hasData = false;
   public statusIntervals?: StatusInterval[];
+
+  constructor(
+    public id: string,
+    public label: string,
+    public url: string,
+    public uom: string,
+    public internalId: string,
+    public firstValue: FirstLastValue,
+    public lastValue: FirstLastValue,
+    public referenceValues: ReferenceValue[],
+    public station: Station,
+    public parameters: ParameterConstellation,
+    public renderingHints: RenderingHints
+  ) { }
+
 }
 
 export interface TimeseriesExtras {
-    license?: string;
-    statusIntervals?: StatusInterval[];
+  license?: string;
+  statusIntervals?: StatusInterval[];
 }
 
 export interface StatusInterval {
-    lower: string;
-    upper: string;
-    name: string;
-    color: string;
+  lower: string;
+  upper: string;
+  name: string;
+  color: string;
 }
 
 export interface PlatformParameter extends Parameter {
-    platformType: PlatformTypes;
+  platformType: PlatformTypes;
 }
 
 export class TimeseriesData {
-  public id!: string;
-  public url!: string;
-  public data!: FirstLastValue[];
+
+  constructor(
+    public id: string,
+    public url: string,
+    public data: FirstLastValue[]
+  ) { }
+
 }
