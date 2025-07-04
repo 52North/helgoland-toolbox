e35d858da4522c2f4b87dc049c63e6e49d076d88
Jan Schulte <j.schulte@52north.org>
2025-07-04 12:09:59 +0200

    fix vulnerabilites

--- scripts/bumpVersions.ts
+++ scripts/bumpVersions.ts
@@ -4,13 +4,20 @@ import { modules } from './utils';
 
 readFile('./package.json', (err, packageMain) => {
   console.log(`bump to version ${packageMain.version}`);
-  modules.forEach(lib => {
+  modules.forEach((lib) => {
     const packageFile = './projects/helgoland/' + lib + '/package.json';
     readFile(packageFile, (e, packageLib) => {
       packageLib.version = packageMain.version;
-      writeFile('./projects/helgoland/' + lib + '/package.json', packageLib, { spaces: 2 }, (error) => {
-        if (error) { console.error(error); }
-      });
+      writeFile(
+        './projects/helgoland/' + lib + '/package.json',
+        packageLib,
+        { spaces: 2 },
+        (error) => {
+          if (error) {
+            console.error(error);
+          }
+        },
+      );
     });
   });
 });
